2552d9bf8177ce3d9068d8785250ccb1d7bbb709
John Wardrop <jwardrop85@gmail.com>
2019-07-18 17:09:02 +0100

    Update Properties

--- env/compute.tf
+++ env/compute.tf
@@ -70,9 +70,9 @@ resource "azurerm_virtual_machine_extension" "InitVM" {
   location             = "${var.g-location}"
   resource_group_name  = "${azurerm_resource_group.rg-main.name}"
   virtual_machine_name = "${azurerm_virtual_machine.vm-hddtest-server2016-prd.name}"
-  publisher            = "Microsoft.Azure.Extensions"
-  type                 = "CustomScript"
-  type_handler_version = "2.0"
+  publisher            = "Microsoft.Compute"
+  type                 = "CustomScriptExtension"
+  type_handler_version = "1.9"
   depends_on = ["azurerm_virtual_machine.vm-hddtest-server2016-prd" ]
 
   settings = <<SETTINGS
